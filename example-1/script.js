d1f49b707c23bdf84500b3bc75d4535c205e15e7
Mitchell <soggybag@gmail.com>
2021-09-22 07:55:47 -0700

    Initial commit

---
+++ example-1/script.js
@@ -0,0 +1,13 @@
+function replace_text() {
+  // TODO: Complete this function to change the color of the h1 tag:
+
+  // STEP 1: Get the h1 element as a variable using document.querySelector.
+
+  // STEP 2: Get the input element with the ID of "new_text" using document.querySelector.
+
+  // STEP 3: Get the value of the input element using dot notation.
+
+  // At this point, it may be useful to print the color value to the console to make sure your code's working so far.
+
+  // STEP 4: Change the innerHTML property of the h1 element to match the value from step 3 using dot notation.
+}
---
+++ example-2.js
@@ -0,0 +1,54 @@
+// If you have node installed you can run this
+// from the terminal with: node example-2.js
+
+// Create an object
+const my_object = {
+	name: 'Borland',
+	isBoring: true,
+	fun: 0.01,
+	says: 'Meh'
+}
+
+// TODO: Create an object that represents a creature
+// Give it a name like: manticore.
+// Give it at least 4 properties and values like 
+// color, size, name, has/is? make sure you have properites 
+// with values of number, string, and bool
+
+
+
+console.log(my_object)
+
+// TODO: use console.log() to log your object to 
+// the console.
+
+
+console.log('Is boring:', my_object.isBoring)
+console.log('Is name:', my_object.name)
+console.log('fun rating:', my_object.fun)
+console.log('Says:', my_object.says)
+
+// TODO: Print your objects properties to the console
+// with console.log(). 
+
+
+
+// My object needs an update!
+my_object.name = 'Borland Bordon Bearson the Third'
+my_object.fun = '1.01'
+my_object.isBoring = false
+my_object.says = 'Hello and how are you on this fine day!'
+
+
+// TODO: Update the properties of your object. 
+// Change each of the four value to an updated value
+
+
+
+console.log('Is boring:', my_object.isBoring)
+console.log('Is name:', my_object.name)
+console.log('fun rating:', my_object.fun)
+console.log('Says:', my_object.says)
+
+// TODO: Print the updated values of your object to
+// the console:
